ce24450b50d79eac6dee99d22a23ebade88ed322
Farhod Joniqulov <147584594+Addonew@users.noreply.github.com>
2023-11-10 10:38:38 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,29 @@
-void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
+def calculate_water_cost(consumption):
+fixed_amount = 13
+cost = fixed_amount
+remaining_consumption = consumption
 
-    return cost;
-}
+if remaining_consumption > 30:
+cost += 30 * 0.4
+remaining_consumption -= 30
+else:
+return cost + remaining_consumption * 0.4
+
+if remaining_consumption > 20:
+cost += 20 * 0.12
+remaining_consumption -= 20
+else:
+return cost + remaining_consumption * 0.12
+
+if remaining_consumption > 10:
+cost += 10 * 1.4
+remaining_consumption -= 10
+else:
+return cost + remaining_consumption * 1.4
+
+cost += remaining_consumption * 1.5
+return cost
+
+        consumption = float(input("Enter water consumption in cubic metres: "))
+total_cost = calculate_water_cost(consumption)
+print("Total cost is:", total_cost)
