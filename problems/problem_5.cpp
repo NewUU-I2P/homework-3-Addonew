52372c12c9934d6eca51f4afd841d4763d0fcc40
Farhod Joniqulov <147584594+Addonew@users.noreply.github.com>
2023-11-10 10:40:07 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,26 @@
-float problemSolution5(float x, float y, char operation) {
-   float result;
-   // write your code here
+# Function to perform arithmetic operations
+def calculate(op, num1, num2):
+if op == '+':
+return num1 + num2
+        elif op == '-':
+return num1 - num2
+        elif op == '*':
+return num1 * num2
+elif op == '/':
+if num2 != 0:
+return num1 / num2
+else:
+return "Error: Division by zero"
+else:
+return "Error: Invalid operation"
 
-   return result;
-}
+# Reading input from the user
+operation = input("Enter the arithmetic operation (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Performing the calculation
+result = calculate(operation, num1, num2)
+
+# Displaying the result
+print("The result of the operation is:", result)
