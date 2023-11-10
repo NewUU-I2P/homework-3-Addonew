53353ca3c07e6c1c63fd17965e64d3cdcbc8b222
Farhod Joniqulov <147584594+Addonew@users.noreply.github.com>
2023-11-10 10:39:29 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,23 @@
-#include <string>
+def get_description(sex, height):
+if sex.lower() == "male":
+if height < 1.70:
+return "Short"
+elif height >= 1.70 and height < 1.85:
+return "Normal"
+else:
+return "Tall"
+elif sex.lower() == "female":
+if height < 1.60:
+return "Short"
+elif height >= 1.60 and height < 1.75:
+return "Normal"
+else:
+return "Tall"
+else:
+return "Invalid sex input"
 
-std::string problemSolution3(float height, char S) {
-    // write your code here
+sex = input("Enter the person's sex (Male/Female): ")
+height = float(input("Enter the person's height in meters: "))
 
-    // use return to return your result
-    // make use of control flow statements
-}
+result = get_description(sex, height)
+print("Description for the height:", result)
