db9d3d4c33bc6b9a7cb2f86c396777f2947bc6b7
Farhod Joniqulov <147584594+Addonew@users.noreply.github.com>
2023-11-13 11:13:48 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,9 +1,23 @@
-# Read prices of three products
-price1 = float(input("Enter price of product 1: "))
-price2 = float(input("Enter price of product 2: "))
-price3 = float(input("Enter price of product 3: "))
+#include <iostream>
 
-# Determine the highest price using the ternary conditional operator
-highest_price = price1 if (price1 > price2 and price1 > price3) else (price2 if (price2 > price3) else price3)
+int main() {
+    double price1, price2, price3;
 
-print("The highest price is:", highest_price)
+    // Input prices
+    std::cout << "Enter the price of product 1: $";
+    std::cin >> price1;
+
+    std::cout << "Enter the price of product 2: $";
+    std::cin >> price2;
+
+    std::cout << "Enter the price of product 3: $";
+    std::cin >> price3;
+
+    // Using the ternary conditional operator to find the highest price
+    double highestPrice = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
+
+    // Display the result
+    std::cout << "The highest price is: $" << highestPrice << std::endl;
+
+    return 0;
+}
