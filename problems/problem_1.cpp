a3b818a9a2928a5e23332c2032b8a3c1f25d477a
Farhod Joniqulov <147584594+Addonew@users.noreply.github.com>
2023-11-13 11:13:04 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -6,33 +6,32 @@ int main() {
     std::cin >> waterConsumption;
 
     double cost = 13.0;  // Fixed amount
-    double remainingConsumption = waterConsumption; // Remaining consumption to be charged
 
-    if (remainingConsumption > 30) {
+    if (waterConsumption > 30) {
         cost += 30 * 0.4; // Charge for the first 30 cubic meters
-        remainingConsumption -= 30;
+        waterConsumption -= 30;
     } else {
-        cost += remainingConsumption * 0.4;
-        remainingConsumption = 0;
+        cost += waterConsumption * 0.4;
+        waterConsumption = 0;
     }
 
-    if (remainingConsumption > 20) {
+    if (waterConsumption > 20) {
         cost += 20 * 0.12; // Charge for the next 20 cubic meters
-        remainingConsumption -= 20;
+        waterConsumption -= 20;
     } else {
-        cost += remainingConsumption * 0.12;
-        remainingConsumption = 0;
+        cost += waterConsumption * 0.12;
+        waterConsumption = 0;
     }
 
-    if (remainingConsumption > 10) {
+    if (waterConsumption > 10) {
         cost += 10 * 1.4; // Charge for the next 10 cubic meters
-        remainingConsumption -= 10;
+        waterConsumption -= 10;
     } else {
-        cost += remainingConsumption * 1.4;
-        remainingConsumption = 0;
+        cost += waterConsumption * 1.4;
+        waterConsumption = 0;
     }
 
-    cost += remainingConsumption * 1.5; // Charge for every additional cubic meter
+    cost += waterConsumption * 1.5; // Charge for every additional cubic meter
 
     std::cout << "Total cost: $" << cost << std::endl;
 
